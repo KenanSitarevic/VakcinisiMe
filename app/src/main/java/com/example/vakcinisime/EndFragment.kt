1b9144637d8f757f68cc76c961627c9e51bb0d7e
SemirSalaka <59878623+SemirSalaka@users.noreply.github.com>
2021-05-04 23:41:24 +0200

    fragmenti

---
+++ app/src/main/java/com/example/vakcinisime/EndFragment.kt
@@ -0,0 +1,20 @@
+package com.example.vakcinisime
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.vakcinisime.databinding.FragmentStartBinding
+
+
+class EndFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentStartBinding>(layoutInflater, R.layout.fragment_end, container, false)
+        return binding.root
+    }
+
+
+}
